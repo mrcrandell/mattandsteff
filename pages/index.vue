7b68fbc62d4b0aaaf8710517687a5d339b507e32
Matt Crandell <mrcrandell@gmail.com>
2025-02-04 12:18:15 -0500

    Testing new photo path

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@ const { data } = await useFetch('/api/photos')
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
     <div v-for="drawing in data?.blobs" :key="drawing.pathname" class="flex flex-col gap-2">
-      <img :src="`/drawings/${drawing.pathname}`" :alt="drawing.pathname" />
+      <img :src="`/photos/${drawing.pathname}`" :alt="drawing.pathname" />
       <div class="flex items-center justify-between">
         <span>{{ drawing.customMetadata?.userName }}</span>
         <span class="text-xs text-gray-500">{{ drawing.uploadedAt }}</span>
