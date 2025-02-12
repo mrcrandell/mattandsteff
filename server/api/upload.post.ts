f98cc37f84598dbe5f11b3b45181aed4e26f5d62
Matt Crandell <mrcrandell@gmail.com>
2025-02-11 22:09:40 -0500

    ahh

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -2,8 +2,8 @@ export default eventHandler(async (event) => {
   // Get token from event data
   const form = await readFormData(event)
   // const token = (form.get('token') || form.get('cf-turnstile-response') || '') as string;
-  const token = form.get('token') as string
-  console.log(token);
+  // const token = form.get('token') as string
+  console.log(form);
   return;
   /* const turnstileResp = await verifyTurnstileToken(token);
   if (!turnstileResp.success) {
