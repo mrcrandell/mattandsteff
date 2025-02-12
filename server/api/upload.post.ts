8b6bc42bc0dbc4e963f396d955b9bf1cfd507004
Matt Crandell <mrcrandell@gmail.com>
2025-02-11 21:50:47 -0500

    debugging

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -3,7 +3,7 @@ export default eventHandler(async (event) => {
   const form = await readFormData(event)
   const token = (form.get('token') || form.get('cf-turnstile-response') || '') as string;
   console.log(token);
-  const turnstileResp = await verifyTurnstileToken(token);
+  /* const turnstileResp = await verifyTurnstileToken(token);
   if (!turnstileResp.success) {
     return {
       status: 403,
@@ -12,6 +12,7 @@ export default eventHandler(async (event) => {
       }
     }
   }
+  */
   return hubBlob().handleUpload(event, {
     formKey: 'photos', // read file or files form the `formKey` field of request body (body should be a `FormData` object)
     multiple: true, // when `true`, the `formKey` field will be an array of `Blob` objects
